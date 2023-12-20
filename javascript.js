39bc25273778b551fdb414c580c1dbdc8f98b90f
aya-m-98 <106470807+aya-m-98@users.noreply.github.com>
2023-12-20 14:17:06 +0200

    final upload

--- javascript.js
+++ javascript.js
@@ -1,42 +1,30 @@
-let slideBtnLeft = document.getElementById("slide-btn-left");
-let slideBtnRight = document.getElementById("slide-btn-right");
-let imgItem = document.querySelectorAll(".image-item");
+var i = 0;
+var ima = [];
+var time = 3000;
 
-// console.log(imgItem.length - 1)
+//image list
+ima[0] = "https://m.media-amazon.com/images/I/71cp9PVuTfL._SX3000_.jpg";
+ima[1] = "https://m.media-amazon.com/images/I/71qlKqpJnlL._SX3000_.jpg";
+ima[2] = "https://m.media-amazon.com/images/I/71cQMXCLSvL._SX3000_.jpg";
+ima[3] = "https://m.media-amazon.com/images/I/61aURrton0L._SX3000_.jpg";
+ima[4] = "https://media.istockphoto.com/id/1305033092/photo/floating-womens-sunglasses-on-a-yellow-background.jpg?s=1024x1024&w=is&k=20&c=GbQsx9zb4nhOQTuabfR9fc3DRKk7SgiF_ihJXkd5Tp0=";
+ima[5] = "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+ima[6] = "https://m.media-amazon.com/images/I/61O72XhcEkL._SX3000_.jpg";
+ima[7] = "https://images.pexels.com/photos/1639729/pexels-photo-1639729.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
-let startSlider = 0;
-let endSlider = (imgItem.length - 1) * 100; // 700
+//function
 
-slideBtnLeft.addEventListener("click", handleLeftBtn);
+function change_img(){
+    document.slide.src = ima[i];
 
-function handleLeftBtn() {
-  if (startSlider < 0) {
-    startSlider = startSlider + 100;
-  }
-  imgItem.forEach((element) => {
-    element.style.transform = `translateX(${startSlider}%)`;
-  });
+    if(i < ima.length - 1){
+        i++;
+    }else{
+        i = 0;
+    }
+    setTimeout("change_img()",time);
 }
-
-slideBtnRight.addEventListener("click", handleRightBtn);
-
-function handleRightBtn() {
-  if (startSlider >= -endSlider + 100) {
-    startSlider = startSlider - 100;
-  }
-  imgItem.forEach((element) => {
-    element.style.transform = `translateX(${startSlider}%)`;
-  });
-}
-//render automatic
-function renderSlideAuto() {
-  if (startSlider >= -endSlider + 100) {
-    handleRightBtn();
-  } else {
-    startSlider = 0;
-  }
-}
-setInterval(renderSlideAuto, 2000);
+window.onload = change_img()
 
 /***** sidebar navigation  */
 const sidebarNavigationEl = document.getElementById("sidebar-id");
